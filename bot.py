4750b7a8b72268363e26f59d3b4e4dc26bd5a320
wotanii <git@wotanii.de>
2017-10-07 13:23:35 +0200

    * implemented #6 * tidied generation of reply-message * added some env-vars * added 2nd compose file for dev

--- bot.py
+++ bot.py
@@ -82,20 +82,30 @@ def upload_file(locale_file_name):
     raise RuntimeError("could not upload file")
 
 
-def generate_reply(uploaded_url, proc_time, upload_time, over_18):
-    nsfw_tag = "# --- NSFW --- \n\n " if over_18 else ""
+def generate_reply(uploaded_url, proc_time, upload_time, over_18, cache_hit):
+    nsfw_note = "# --- NSFW --- \n\n " if over_18 else ""
 
-    return (nsfw_tag +
-            "I have stabilized the video for you: " + uploaded_url + " \n\n"
-            "It took " + "%.f" % proc_time + " seconds to process "
-            "and " +  "%.f" % upload_time + " seconds to upload.\n"
-            "___\n"
-            "[^^how ^^to ^^use]"
-            "(https://www.reddit.com/r/stabbot/comments/72irce/how_to_use_stabbot/)"
-            " ^^| [^^programmer](https://www.reddit.com/message/compose/?to=wotanii)"
-            " ^^| [^^source ^^code](https://gitlab.com/wotanii/stabbot)"
-            " ^^| ^^/r/ImageStabilization/"
-            " ^^| ^^for ^^cropped ^^results, ^^use ^^\/u/stabbot_crop")
+    result_note = "I have stabilized the video for you: " + uploaded_url
+
+    if cache_hit:
+        time_note = ""
+    else:
+        time_note = "\n\nIt took " + "%.f" % proc_time + " seconds to process "\
+                        "and " +  "%.f" % upload_time + " seconds to upload.\n"
+
+    foot_note = "[^^how ^^to ^^use]"\
+                "(https://www.reddit.com/r/stabbot/comments/72irce/how_to_use_stabbot/)"\
+                " ^^| [^^programmer](https://www.reddit.com/message/compose/?to=wotanii)"\
+                " ^^| [^^source ^^code](https://gitlab.com/wotanii/stabbot)"\
+                " ^^| ^^/r/ImageStabilization/"\
+                " ^^| ^^for ^^cropped ^^results, ^^use ^^\/u/stabbot_crop"\
+
+    return nsfw_note\
+        + result_note\
+        + uploaded_url\
+        + time_note\
+        + "___\n"\
+        + foot_note
 
 
 def clear_env():
@@ -105,63 +115,44 @@ def clear_env():
     os.chdir(woring_path)
 
 
-def mark_submission(post_id, posts_replied_to):
-    if debug:
-        print "marking submission " + post_id + " ... "
-    posts_replied_to.append(post_id)
-    with open(posts_replied_to_path, "a") as f:
-        f.write(post_id + "\n")
-
-
-def get_replied_to_list():
-    if os.path.isfile(posts_replied_to_path):
-        with open(posts_replied_to_path, "r") as f:
-            file_content = f.read()
-            file_content = file_content.split("\n")
-            return list(filter(None, file_content))
-
-    return []
-
-
-def get_next_job(posts_replied_to):
+def get_next_job():
     for mention in reddit.inbox.mentions(limit=50):
         if not mention.new and not include_old_mentions:
             continue
         if not dryrun:
             mention.mark_read()
-        if not mention.is_root:
-            print "comment is not root: ", mention
-            continue
-        if mention.submission.id in posts_replied_to:
-            print "submission was alread replied to: " + mention.submission.id
-            continue
 
         return mention
 
+def check_cache(input_path):
+    input_md5 = hashlib.md5(open(input_path, 'rb').read()).hexdigest()
+    return r.get("md5-" + input_md5)
+
+def set_cache(uploaded_url, input_path):
+    input_md5 = hashlib.md5(open(input_path, 'rb').read()).hexdigest()
+    r.set("md5-" + input_md5, uploaded_url)
 
 def main():
     print "starting..."
-    posts_replied_to = get_replied_to_list()
-    print "old replies found: " + str(len(posts_replied_to))
     while True:
         try:
             clear_env()
-            mention = get_next_job(posts_replied_to)
+            mention = get_next_job()
             if mention is None:
-                if debug:
-                    print "No Mention found. Going back to sleep ..."
                 time.sleep(sleep_time_s)
                 continue
-            mark_submission(mention.submission.id, posts_replied_to)
             print "submission: " + mention.submission.id + " - " + mention.submission.shortlink
             start_time = time.time()
 
             input_path = search_and_download_video(mention.submission)
-            stab_file(input_path, "stabilized.mp4")
-            proc_time = time.time() - start_time
-            uploaded_url = upload_file('stabilized.mp4')
+            cached_result = check_cache(input_path)
+            if(cached_result is None):
+                stab_file(input_path, "stabilized.mp4")
+                proc_time = time.time() - start_time
+                uploaded_url = upload_file('stabilized.mp4')
+                set_cache(uploaded_url, input_path)
             upload_time = time.time() - start_time - proc_time
-            reply_md = generate_reply(uploaded_url, proc_time, upload_time, mention.submission.over_18)
+            reply_md = generate_reply(uploaded_url, proc_time, upload_time, mention.submission.over_18, cached_result is not None)
 
         except Exception as e:
             print "Exception:"
